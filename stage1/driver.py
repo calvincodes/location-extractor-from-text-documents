729e02b3eeda9861568c3137b9a8abc4f1d2a19f
calvincodes <arpitjain1821@gmail.com>
2019-03-09 09:43:54 -0600

    Persist all feature vectors in one single variable

--- stage1/driver.py
+++ stage1/driver.py
@@ -6,11 +6,13 @@ from feature_vector_converter import convert_examples_to_feature_vectors
 data_set_directory = 'all_labelled_data_set/'
 dirname = os.path.dirname(__file__)
 
-# for file_no in [163]:
+global_feature_vector_list = []
+
 for file_no in [318, 325, 297, 335, 163, 124]:
     filename = os.path.join(dirname, data_set_directory+str(file_no)+".txt")
     word_list = read_file(filename)
     examples = generate_examples(word_list)
     feature_vectors = convert_examples_to_feature_vectors(word_list, examples)
+    global_feature_vector_list.extend(feature_vectors)
     print(str(examples.positive))
 
