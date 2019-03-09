8d0649c34c8c3e36a1e8e30e49e3a1e1ec23aa21
calvincodes <arpitjain1821@gmail.com>
2019-03-09 03:32:33 -0600

    Renaming variables in input_reader

--- stage1/input_reader.py
+++ stage1/input_reader.py
@@ -1,6 +1,6 @@
 def read_file(filename):
     stream = open(filename, "r")
     raw_data_set = stream.read()
-    raw_data = raw_data_set.split()
-    word_list = [word.strip(" .,;:()") for word in raw_data]
-    return word_list
+    raw_data_list = raw_data_set.split()
+    sanitized_word_list = [word.strip(" .,;:()") for word in raw_data_list]
+    return sanitized_word_list
