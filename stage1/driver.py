70755ed2e05ce871a6f6acf35aeb9a5e8344e78b
calvincodes <arpitjain1821@gmail.com>
2019-03-09 20:37:25 -0600

    Saving workspace

--- stage1/driver.py
+++ stage1/driver.py
@@ -9,7 +9,12 @@ data_set_directory = 'all_labelled_data_set/'
 dirname = os.path.dirname(__file__)
 
 global_data_frames = []
+all_positive_examples = []  # Used only for debugging purpose
+all_negative_examples = []  # Used only for debugging purpose
 
+pos_count = 0
+
+# for file_no in range(154):
 for file_no in range(117, 370):
 
     # Step 1: Create absolute path for the file and check if the file exists.
@@ -22,9 +27,17 @@ for file_no in range(117, 370):
 
     # Step 3: Generate both positive and negative, uni-gram and bi-gram examples from the dataset.
     examples = generate_examples(word_list)
+    all_positive_examples.extend(examples.positive)
+    all_negative_examples.extend(examples.negative)
 
     # Step 4: Convert the examples into feature vectors.
     positive_feature_vectors, negative_feature_vectors = convert_examples_to_feature_vectors(word_list, examples)
+
+    # pos_count += len(examples.positive)
+    # if pos_count < 70:
+    #     all_positive_examples.extend(examples.positive)
+    #     positive_data_frames = generate_data_frame(examples.positive, positive_feature_vectors, 1)
+    #     global_data_frames.extend(positive_data_frames)
 
     # Step 5: Convert the feature vectors into panda DataFrames which can be fed to sci-kit.
     positive_data_frames = generate_data_frame(examples.positive, positive_feature_vectors, 1)
@@ -34,4 +47,6 @@ for file_no in range(117, 370):
     global_data_frames.extend(positive_data_frames)
     global_data_frames.extend(negative_data_frames)
 
+print("Positive cases = " + str(len(all_positive_examples)))
+print("Negative cases = " + str(len(all_negative_examples)))
 train_all_models(global_data_frames)
--- stage1/example_generator.py
+++ stage1/example_generator.py
@@ -1,11 +1,12 @@
 import re
 import random
 from examples import Examples
-from constants import blacklisted_rule_words
+from constants import blacklisted_rule_words, whitelisted_words, neighboring_verbs_for_negative_examples
 
 
 def generate_examples(word_list):
 
+    lowered_whitelisted_words = [x.lower() for x in whitelisted_words]
     examples = Examples()
 
     for i, word in enumerate(word_list):
@@ -19,7 +20,7 @@ def generate_examples(word_list):
         if '<loc>' in word:
             # For the same reason as above, explicitly checking for one occurrence of both opening and closing tags.
             if word.count("<loc>") == 1 and word.count("</loc>") == 1:
-                extracted_word = re.sub('<[^>]*>|[^a-zA-Z\d\s:]', '', word)
+                extracted_word = re.sub('<[^>]*>', '', word)
                 examples.positive.append([i, extracted_word])
             else:
                 # If the label spans across multiple words, like <loc>United States of America</loc>.
@@ -28,7 +29,7 @@ def generate_examples(word_list):
                     if word_list[tag_closing_index].count("</loc>") == 1:
                         tagged_substring_list = word_list[i:tag_closing_index+1]
                         tagged_substring = ''.join(str(e + " ") for e in tagged_substring_list)
-                        extracted_word = re.sub('<[^>]*>|[^a-zA-Z\d\s:]', '', tagged_substring).strip()
+                        extracted_word = re.sub('<[^>]*>', '', tagged_substring).strip()
                         examples.positive.append([i, extracted_word])
                         for x in range(i+1, tag_closing_index+1):
                             word_list[x] = "__"
@@ -38,25 +39,32 @@ def generate_examples(word_list):
             # Only the words with first letter upper, and not in the rule list will be used for uni-gram formation.
             # The formation of bi-gram will be on the basis of the second word. If second word is not loc
             # and is not a rule word, then the bi-gram is created.
-            if word[0].isupper() \
-                    and word.lower() not in blacklisted_rule_words \
-                    and not (any(ch.isdigit() for ch in word)):
-                # Adding probabilistic pickup to ensure negatives do not overshoot positives by a significant number
-                # FIXME: Note that this will result in changing the training data in every run of the model.
-                # FIXME: I am NON-DETERMINISTIC
-                if random.random() < 0.12:
-                    examples.negative.append([i, word])
+            # if word[0].isupper() and word.lower() not in lowered_whitelisted_words:
+            #     examples.negative.append([i, word])
+            #     next_word = word_list[i + 1] if i < len(word_list) - 1 else "__"
+            #     if '<loc>' not in next_word:
+            #         examples.negative.append([i, word + " " + next_word])
 
-                # **************** Create bi-gram examples ****************
-                # If condition to define next_word, depending on current word is last or not.
+            if word[0].isupper() and word.lower() in lowered_whitelisted_words:
+                prev_prev_word = word_list[i - 2] if i > 0 else "__"
+                prev_word = word_list[i - 1] if i > 0 else "__"
                 next_word = word_list[i + 1] if i < len(word_list) - 1 else "__"
-                if next_word.lower() not in blacklisted_rule_words \
-                        and '<loc>' not in next_word \
-                        and not (any(ch.isdigit() for ch in next_word)):
-                    # FIXME: Note that this will result in changing the training data in every run of the model.
-                    # FIXME: I am NON-DETERMINISTIC
-                    if random.random() < 0.1:
-                        examples.negative.append([i, word + " " + next_word])
+                next_next_word = word_list[i + 2] if i < len(word_list) - 2 else "__"
+
+                if '<loc>' not in prev_word and prev_word[0].isupper():
+                    examples.negative.append([i-1, prev_word + " " + word])
+
+                if '<loc>' not in prev_word and prev_word.lower() in neighboring_verbs_for_negative_examples:
+                    examples.negative.append([i-1, prev_word + " " + word])
+                    if '<loc>' not in prev_prev_word:
+                        examples.negative.append([i-2, prev_prev_word + " " + prev_word + " " + word])
+
+                if '<loc>' not in next_word and next_word[0].isupper():
+                    examples.negative.append([i, word + " " + next_word])
+
+                if '<loc>' not in next_word and next_word.lower() in neighboring_verbs_for_negative_examples:
+                    examples.negative.append([i, word + " " + next_word])
+                    if '<loc>' not in next_next_word:
+                        examples.negative.append([i, word + " " + next_word + " " + next_next_word])
 
     return examples
-
