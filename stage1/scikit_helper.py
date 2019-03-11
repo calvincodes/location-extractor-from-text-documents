d528c3db9f9442e5f9fb501b53e46b54257a2e44
calvincodes <arpitjain1821@gmail.com>
2019-03-10 19:13:25 -0500

    Removing redundant codes and comments

--- stage1/scikit_helper.py
+++ stage1/scikit_helper.py
@@ -1,6 +1,6 @@
 import sklearn
 
-from sklearn import svm, metrics
+from sklearn import svm
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import LinearRegression, LogisticRegression
 from sklearn.model_selection import cross_val_score
